16c0b4e9d4903366eb39e61a8585fcef9072da47
Alec Teihotaata <teihotaata.alec@gmail.com>
2018-07-25 16:47:58 +0200

    ajout du logo et bugfix des espacements

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
 <!--scripts-->
 
+<div class="logoContainer">
+    <a href="<?= home_url(); ?>"><img src="<?php echo get_template_directory_uri() ?>/images/logo.png" alt="logo"></a>
+</div>
 <script src="<?php echo get_template_directory_uri()?>/js/TweenMax.js"></script>
 <?php wp_footer(); ?>
 
